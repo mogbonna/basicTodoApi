aa5f595b9d83a2ad7bf85c15b6999f7aaa10a98e
mogbonna <mogbonna045@gmail.com>
2024-12-24 23:56:53 +0100

    Done with todoRoute

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const TodoController = require("../controllers/todo.controller");
+const TodoValidator = require("../middleware/validators");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(TodoController.getAllTodos)
+  .post(TodoValidator.create, TodoController.createTodo);
+
+router
+  .route("/:id")
+  .get(TodoController.getTodoById)
+  .put(TodoValidator.update, TodoController.updateTodo)
+  .delete(TodoController.deleteTodo);
+
+module.exports = router;
